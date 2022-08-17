436e60b1e6d56442116787db1f66a97662f8cc28
LeeDongjun <adgjl1125@neowoori.com>
2022-08-17 21:02:21 +0900

    소스 수정

--- practical_exam/src/main/java/com/practical/exam/home/HomeController.java
+++ practical_exam/src/main/java/com/practical/exam/home/HomeController.java
@@ -1,12 +1,12 @@
 package com.practical.exam.home;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
-	@PostMapping("/")
+	@GetMapping("/")
 	public String homeIndex() {
 		return "hello";
 	}
