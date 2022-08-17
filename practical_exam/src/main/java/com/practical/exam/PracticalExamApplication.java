77b3c5d94aff307e806943f72003c85197d2eb16
LeeDongjun <adgjl1125@neowoori.com>
2022-08-17 20:21:50 +0900

    sss

---
+++ practical_exam/src/main/java/com/practical/exam/PracticalExamApplication.java
@@ -0,0 +1,13 @@
+package com.practical.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalExamApplication.class, args);
+	}
+
+}
---
+++ practical_exam/src/test/java/com/practical/exam/PracticalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practical.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
