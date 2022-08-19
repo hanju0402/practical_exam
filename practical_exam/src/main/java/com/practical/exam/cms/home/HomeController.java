08df7113e028d087575733103efcd07b0d42e544
LeeDongjun <adgjl1125@neowoori.com>
2022-08-19 16:47:26 +0900

    세션 수정..

--- practical_exam/src/main/java/com/practical/exam/cms/home/HomeController.java
+++ practical_exam/src/main/java/com/practical/exam/cms/home/HomeController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.practical.exam.cms.home.service.HomeService;
-import com.practical.exam.common.auth.SessionUtils;
 import com.practical.exam.common.auth.UserInfo;
 
 @Controller
@@ -21,22 +20,19 @@ public class HomeController {
 	@Autowired
 	HomeService homeService;
 	
-	@Autowired
-	SessionUtils session;
-	
 	@Resource
 	private UserInfo userInfo;
 	/**
 	 * 로그인 페이지 이동
-	 * 차후, '/' URL로 접근 시, 세션 여부 확인 후, LOGIN 페이지 OR 메인 페이지로 노출하는 로직 개발 필요
+	 * 해당 URL로 접근 시, 세션 여부 확인 후, 
+	 * LOGIN 페이지 OR 메인 페이지로 노출하는 로직 개발 필요
 	 * 
 	 * @return LOGIN 화면(세션x) , MainPage(세션o)
 	 */
-	@RequestMapping(value="/", method = RequestMethod.POST)
+    @RequestMapping(value="/", method = {RequestMethod.POST, RequestMethod.GET})
 	public ModelAndView loginPage() {
 		ModelAndView mav = new ModelAndView();
-		
-		if(session.sessionExist()) {
+		if(userInfo.getUserId() == null) {
 			System.out.println("세션 없음");
 			mav.setViewName("/home/home");			
 		} else {
@@ -54,7 +50,7 @@ public class HomeController {
 	 * @return
 	 */
 	@RequestMapping(value="/login", method = RequestMethod.POST)
-	public String login(@RequestParam HashMap<String,Object> params) {
+	public String login(@RequestParam HashMap<String,String> params) {
 		homeService.login(params);
 		return "redirect:/";
 	}
--- practical_exam/src/main/java/com/practical/exam/cms/home/service/HomeService.java
+++ practical_exam/src/main/java/com/practical/exam/cms/home/service/HomeService.java
@@ -2,35 +2,37 @@ package com.practical.exam.cms.home.service;
 
 import java.util.HashMap;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
-import com.practical.exam.common.auth.SessionUtils;
+import com.practical.exam.common.auth.UserInfo;
 
 @Service("homeService")
 public class HomeService {
 	
-	@Autowired
-	SessionUtils session;
+
+	@Resource
+	private UserInfo userInfo;
 	/**
 	 * 로그인 처리 
 	 * 
 	 * @param reqData
 	 * @return
 	 */
-	public void login(HashMap<String,Object> reqData) {
+	public void login(HashMap<String,String> reqData) {
 		// DB가 없으므로 , 'sss' ,'ddd'로 하드코딩 되어있음.
 		String id="sss";
 		String pw="ddd";
 		
-		String reqUserId= reqData.get("username").toString();
-		String reqUserpw= reqData.get("password").toString();
+		String reqUserId= reqData.get("userId");
+		String reqUserpw= reqData.get("password");
 		
 		// 로그인 성공 시,
 		if(id.equals(reqUserId) && pw.equals(reqUserpw)) {
 			// 세션 생성
-			HashMap<String,String> reqInfo = new HashMap<String,String>();
-			session.setSession(reqInfo);
+			userInfo.setUserId(reqUserId);
+			userInfo.setUserNm(reqUserpw);
 		} 
 	}
 }
