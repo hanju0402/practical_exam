fd67d6da280b1d566b6e8fae4ca8affa8d80d9e6
이동준 <adgjl1125@neowoori.com>
2022-09-23 16:17:49 +0900

    불필요한 소스 제거

--- practical_exam/src/main/java/com/practical/exam/cms/examination/ExaminationController.java
+++ practical_exam/src/main/java/com/practical/exam/cms/examination/ExaminationController.java
@@ -40,7 +40,6 @@ public class ExaminationController {
 		if(reqData.get("hiddenData") == null) {
 			mav.setViewName("redirect:/examination");
 		} else {
-			System.out.println("히든데이터???  " + reqData.get("hiddenData"));
 			mav.setViewName("/examination/shamExam");
 			mav.addObject("examList",examinationService.getExamination());
 		}
@@ -56,23 +55,8 @@ public class ExaminationController {
 	
 	@RequestMapping(value = "/marking", method = RequestMethod.POST)
 	public ModelAndView marking(@RequestBody List<String> params) {
-		String userId = examinationService.getUserId();
-		int testCnt = examinationService.getTestCnt(userId);
-		List<Map<String,Object>> seq = examinationService.getAnswerSeq(userId, testCnt);
-		List<String> answer = examinationService.getAnswer(seq);
 		ModelAndView mav = new ModelAndView();
-				
 		mav.setViewName("/examination/marking");
-		System.out.println("seq list??" + seq);
-		System.out.println("seq intdex??" + (Integer)seq.get(0).get("seq"));
-		System.out.println("현재 회차는???" + testCnt);
-		System.out.println("현재 아이디는???" + userId);
-		System.out.println("정답아레아???  " + params);
-		System.out.println("문제정답 리스트" + answer);
-		System.out.println("1번문제 제출답??" + params.get(0));
-		System.out.println("1번문제 실제정답??" + answer.get(0));
-		
-		
 		return mav;
 	}
 	
